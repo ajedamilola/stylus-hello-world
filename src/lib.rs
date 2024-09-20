fa7f083acdfef14428dacb30625bb4863528b212
Aje Damilola <ajedamilola2005@gmail.com>
2024-09-20 15:26:51 +0100

    Some changes

--- src/lib.rs
+++ src/lib.rs
@@ -24,45 +24,31 @@
 // Allow `cargo stylus export-abi` to generate a main function.
 #![cfg_attr(not(feature = "export-abi"), no_main)]
 extern crate alloc;
-
-/// Import items from the SDK. The prelude contains common traits and macros.
 use stylus_sdk::{alloy_primitives::U256, prelude::*};
 
-// Define some persistent storage using the Solidity ABI.
-// `Counter` will be the entrypoint.
 sol_storage! {
     #[entrypoint]
-    pub struct Counter {
+    pub struct Counter{
         uint256 number;
     }
 }
 
-/// Declare that `Counter` is a contract with the following external methods.
 #[public]
 impl Counter {
-    /// Gets the number from storage.
-    pub fn number(&self) -> U256 {
-        self.number.get()
+    pub fn number(&mut self) -> Result<U256, Vec<u8>> {
+        Ok(self.number.get())
+    }
+    pub fn set_number(&mut self, number: U256) {
+        self.number.set(number);
     }
 
-    /// Sets a number in storage to a user-specified value.
-    pub fn set_number(&mut self, new_number: U256) {
-        self.number.set(new_number);
-    }
-
-    /// Sets a number in storage to a user-specified value.
-    pub fn mul_number(&mut self, new_number: U256) {
-        self.number.set(new_number * self.number.get());
-    }
-
-    /// Sets a number in storage to a user-specified value.
-    pub fn add_number(&mut self, new_number: U256) {
-        self.number.set(new_number + self.number.get());
-    }
-
-    /// Increments `number` and updates its value in storage.
     pub fn increment(&mut self) {
-        let number = self.number.get();
-        self.set_number(number + U256::from(1));
+        let former = self.number().unwrap();
+        self.number.set(former + U256::from(1));
+    }
+
+    pub fn decrement(&mut self) {
+        let former = self.number().unwrap();
+        self.number.set(former - U256::from(1));
     }
 }
